ed1399620e23d3dbecce055315f97b824cffdbad
Jorge-Sarabia <jorge@semantic.mx>
2025-07-25 00:21:47 -0600

    chore: update Jest setup and dependencies

--- jest.setup.js
+++ jest.setup.js
@@ -13,10 +13,10 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
-// Mock AsyncStorage
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
-);
+// Mock AsyncStorage (commented out since we don't use it yet)
+// jest.mock('@react-native-async-storage/async-storage', () =>
+//   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+// );
 
 // Silence the warning: Animated: `useNativeDriver` is not supported
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
